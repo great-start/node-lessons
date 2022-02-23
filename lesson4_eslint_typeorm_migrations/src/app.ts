329df02d1719b7c05b48bc591a001722a555ee63
ev <zhenya.bill@gmail.com>
2022-02-23 18:28:48 +0200

    lesson2

--- lesson4_eslint_typeorm_migrations/src/app.ts
+++ lesson4_eslint_typeorm_migrations/src/app.ts
@@ -1,5 +1,11 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('');
+app.get('/', (req:Request, res:Response) => {
+    res.end();
+});
+
+app.listen(5200, () => {
+    console.log('Server has started!!!YEAH!!!');
+});
